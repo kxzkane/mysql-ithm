c3cb328cdbc07b8796c92a519831ef944c14a56b
ikun <123456@.com>
2019-10-25 01:19:24 +0800

    1.0版本发布

--- README.md
+++ README.md
@@ -1,2 +1,202 @@
+![](README.assets/logo.png)
+
 # mysql-ithm
+
 一款nodejs操作mysql数据库的orm库
+
+* 黑马程序员：[http://www.itheima.com](http://www.itheima.com/)
+* 官方文档地址：https://github.com/kxzkane/mysql-ithm
+
+* 技术亮点
+  * (1)基于ORM技术，使用对象的方式来操作mysql数据库，而无需编写任何sql语句
+  * (2)支持数据库的常规操作`增删改查`，API极为简洁，且高效智能。
+  * (3)支持条件查询 与 分页查询
+  * (4)支持原生sql语句，如果我们的框架无法满足您的需求，我们也提供了特殊的API可以直接使用sql语句来操作
+  * (5)支持链式语法
+
+# 01-安装
+
+* 首先需要确保你的项目已经安装了mysql
+  * `npm install mysql`
+* 安装`mysql-ithm`
+  * `npm install ithm`
+
+
+
+# 02-导入
+
+```javascript
+//导入模块
+const mysql = require('mysql');
+const hm = require('mysql-ithm');
+```
+
+
+
+# 03-使用(example)
+
+```javascript
+//1.导入模块
+const mysql = require('mysql');
+const hm = require('mysql-ithm');
+
+//2.连接数据库
+hm.connect({
+    host: 'localhost',//数据库地址
+    port:'3306',
+    user: 'root',//用户名，没有可不填
+    password: 'root',//密码，没有可不填
+    database: 'hm'//数据库名称
+});
+
+//3.创建Model(表格模型：负责增删改查)
+let studentModel = hm.model('student',{
+    name:String,
+    age:Number
+});
+
+//4.调用API：添加数据
+studentModel.insert({name:'张三10',age:30},(err,results)=>{
+    console.log(err);
+    console.log(results);
+    if(!err) console.log('增加成功');
+});
+```
+
+
+
+# 04-API Document
+
+## 1.1-增加操作
+
+
+
+```javascript
+studentModel.insert({name:'张三10',age:30},(err,results)=>{
+    console.log(err);
+    console.log(results);
+    if(!err) console.log('增加成功');
+});
+```
+
+
+
+##  1.2-查询操作
+
+### 1-查询所有数据
+
+```javascript
+//2.1 查询所有数据
+studentModel.find((err,results)=>{
+    console.log(results);
+});
+```
+
+### 2-查询数据库指定字段数据
+
+```javascript
+//2.2 根据数据库字段查询部分数据
+// ['name'] : 将要查询的字段放入数组中
+studentModel.find(['name'],(err,results)=>{
+    console.log(results);
+});
+```
+
+### 3-条件查询
+
+```javascript
+//2.3 根据条件查询数据
+// 'id=1' : 查询id为1的数据 (查询条件可以参考sql语句)
+//例如 'age>10' : 查询age超过10的数据 
+//例如 'name>"张三"' : 查询名字为张三的数据，注意字符串添加引号
+studentModel.find('id>21',(err,results)=>{
+    console.log(results);
+});
+```
+
+### 4-分页查询
+
+```javascript
+//2.4 分页查询
+//  第一个参数options对象有三个属性 {where:分页查询条件（可选）， number:页数 ， count：每页数量}
+studentModel.limit({where:'age>28',number:1,count:10},(err,results)=>{
+    console.log(results);
+});
+```
+
+## 1.3-修改操作
+
+### 1-修改所有数据
+
+```javascript
+//3.1 将数据库中所有的name字段值：修改为李四
+studentModel.update({name:'李四'},(err,results)=>{
+    console.log(results);
+});
+```
+
+### 2-条件修改
+
+```javascript
+//3.2 将数据库中 id = 1 的数据，age修改为30
+studentModel.update('id=1',{age:30},(err,results)=>{
+    console.log(results);
+});
+
+//3.3 将数据库中所有 age < 20 的数据，name修改为王五
+studentModel.update('age<20',{name:'王五'},(err,results)=>{
+    console.log(results);
+});
+```
+
+
+
+## 1.4-删除操作
+
+```javascript
+//4.1 删除所有 age>30 的数据
+studentModel.delete('age>20',(err,results)=>{
+    console.log(results);
+});
+
+//4.2 清空表中所有数据
+studentModel.delete((err,results)=>{
+    console.log(results);
+});
+```
+
+
+
+## 1.5-执行自定义SQL语句
+
+```javascript
+studentModel.sql('insert into student(name,age) values("andy",20)',(err,results)=>{
+    console.log(results);
+});
+```
+
+
+
+## 1.5-删除表格（慎用）
+
+```javascript
+studentModel.drop((err,results)=>{
+    console.log(results);
+});
+```
+
+
+
+## 1.7-链式语法支持
+
+```javascript
+studentModel.insert({name:'张三22',age:22},(err,results)=>{
+    console.log(err);
+    console.log(results);
+})
+.find('name="张三22"',(err,results)=>{
+    console.log(err);
+    console.log(results);
+});
+```
+
---
+++ index.js
@@ -0,0 +1,240 @@
+const mysql = require('mysql');
+
+let connection;
+let insertQueue = {};
+let createQueue = {};
+let databaseName = '';
+
+function Model(name, option) {
+    this.name = name;
+    this.option = option;
+};
+
+
+/**
+* @description: 查询数据
+* @param {} options：可选参数
+* @param {Function} callback :（req,results）=>{}
+*/
+Model.prototype.find = function (options, callback) {
+    var str = '';
+    if (!callback) {
+        str = `select * from ${this.name}`;
+        callback = options;
+    } else if (options.constructor == Array) {
+        str = `select ${options.join()} from ${this.name}`;
+    } else {
+        str = `select * from ${this.name} where ${options}`;
+    };
+    //console.log(str);
+    connection.query(str, (error, results, fields) => {
+        callback(error, results, fields);
+    });
+    return this;
+};
+
+/**
+* @description: 分页查询
+* @param {Object} options :   { where:查询条件, number: 当前页数 , count : 每页数量 }
+* @return: 
+*/
+Model.prototype.limit = function (options, callback) {
+    var str = '';
+    if (!options.where) {
+        str = `select * from ${this.name} limit ${(options.number - 1)*options.count},${options.count}`;
+    }else {
+        str = str = `select * from ${this.name} where ${options.where} limit ${(options.number - 1)*options.count},${options.count}`;
+    };
+    console.log(str);
+    connection.query(str, (error, results, fields) => {
+        callback(error, results, fields);
+    });
+    return this;
+};
+
+/**
+* @description: 插入数据
+* @param {Object} 
+* @param {Function} callback :（req,results）=>{}
+*/
+Model.prototype.insert = function (obj, callback) {
+    //先执行一次，激活数据库
+    let keys = [];
+    let values = '';
+    for (var key in obj) {
+        keys.push(key);
+        values += `"${obj[key]}",`;
+    };
+    values = values.replace(/,$/, '');
+    let str = `INSERT INTO ${this.name} (${keys.join()}) VALUES (${values})`;
+    //console.log(str);
+
+    var model1 = this;
+    connection.query(str, (error, results, fields) => {
+        if (error && !insertQueue.obj) {
+            insertQueue.obj = obj;
+            insertQueue.callback = callback;
+            insertQueue.model = model1;
+        }else{
+            callback(error,results,fields);
+        }
+    });
+    return this;
+};
+
+/**
+* @description: 更新数据
+* @param {Object} option：可选参数 更新条件
+* @param {Object} obj： 修改后的数据 
+* @param {Function} callback :（req,results）=>{}
+*/
+Model.prototype.update = function (option, obj, callback) {
+    let str = '';
+    if (arguments.length == 2) {
+        callback = obj;
+        obj = option;
+        str = `UPDATE ${this.name} SET `;
+        for (var key in obj) {
+            str += `${key}='${obj[key]}', `;
+        };
+        str = str.replace(/(, )$/, '');
+    } else {
+        str = `UPDATE ${this.name} SET `;
+        for (var key in obj) {
+            str += `${key}='${obj[key]}', `;
+        };
+        str = str.replace(/(, )$/, '');
+        str += ` where ${option}`;
+    };
+
+    console.log(str);
+    connection.query(str, (error, results, fields) => {
+        callback(error, results, fields);
+    });
+    return this;
+
+};
+
+/**
+* @description: 删除数据
+* @param {Object} option：可选参数 删除条件
+* @param {Function} callback :（req,results）=>{}
+*/
+Model.prototype.delete = function (option, callback) {
+    var str = '';
+    if (!callback) {
+        str = `delete from ${this.name}`;
+        callback = option;
+    } else {
+        str = `delete from ${this.name} where ${option}`;
+    };
+    console.log(str);
+    connection.query(str, (error, results, fields) => {
+        callback(error, results, fields);
+    });
+    return this;
+};
+
+/**
+* @description: 执行sql语句
+* @param {String} str : sql语句
+* @param {Function} callback :（req,results）=>{}
+*/
+Model.prototype.sql = function (str, callback) {
+    connection.query(str, (error, results, fields) => {
+        callback(error, results, fields);
+    });
+    return this;
+};
+
+/**
+* @description: 删除model表格 （慎用！）
+* @param {type} 
+* @return: 
+*/
+Model.prototype.drop = function (callback) {
+    connection.query(`DROP TABLE ${this.name}`, (error, results, fields) => {
+        callback(error, results, fields);
+    });
+    return this;
+};
+
+let hm = {
+    /**
+    * @description:连接数据库
+    * @param {String} host: 主机名 默认localhost
+    * @param {Number} port: 端口号 默认3306
+    * @param {String} user: 用户名 默认root
+    * @param {String} password: 密码 默认root
+    * @param {String} database: 数据库名称 默认hm
+    * @return: 
+    */
+    connect: function ({ host = 'localhost', port = 3306, user = 'root', password = 'root', database = 'hm' }) {
+        databaseName = database;//全局存储当前数据库名称
+
+        connection = mysql.createConnection({
+            host,//数据库地址
+            port,//端口号
+            user,//用户名，没有可不填
+            password,//密码，没有可不填
+            database//数据库名称
+        });
+        connection.connect((err) => {
+            if (err) {
+                //console.log(connection);
+                connection = mysql.createConnection({
+                    host,//数据库地址
+                    port,//端口号
+                    user,//用户名，没有可不填
+                    password,//密码，没有可不填
+                });
+                connection.connect((err) => {
+                    //if (err) throw error;
+                    connection.query(`CREATE DATABASE ${database}`, (error, results, fields) => {
+                        //if (error) throw error;
+                        connection.query(`use ${database}`, (error, results, fields) => {
+                            if (!error) {
+                                if (createQueue.name) {
+                                    hm.model(createQueue.name,createQueue.options);
+                                }
+                            }
+                        });
+
+                    });
+                });
+            }
+        });
+    },
+    /**
+    * @description:创建model (表格模型对象)
+    * @param {String} name:表格名称
+    * @param {Object} options:表格数据结构
+    * @return: Model对象：负责数据库增删改查
+    */
+    model: function (name, options) {
+        let str = 'id int primary key auto_increment, ';
+        for (var key in options) {
+            if (options[key] == Number) {
+                str += `${key} numeric,`;
+            } else if (options[key] == Date) {
+                str += `${key} timestamp,`;
+            } else {
+                str += `${key} varchar(255),`;
+            }
+        };
+        str = str.replace(/,$/, '');
+        console.log(`CREATE TABLE ${name} (${str})`);
+        //console.log(str);
+        connection.query(`CREATE TABLE ${name} (${str})`, (error, results, fields) => {
+            if (error) {
+                createQueue.name = name;
+                createQueue.options = options;
+            }else{
+                insertQueue.model.insert(insertQueue.obj,insertQueue.callback);
+            }
+        });
+        return new Model(name, options);
+    }
+};
+
+module.exports = hm;
